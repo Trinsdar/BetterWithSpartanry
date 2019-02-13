0caaee0a2d659c3952d4cd1fab54030893c76b0e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-02-13 00:30:48 -0500

    started work on updating to latest spartan weaponry

--- src/main/java/com/chaosbuffalo/betterwithspartanry/init/ItemRegistryBWSpartanry.java
+++ src/main/java/com/chaosbuffalo/betterwithspartanry/init/ItemRegistryBWSpartanry.java
@@ -4,21 +4,44 @@ import betterwithmods.common.BWMItems;
 import com.chaosbuffalo.betterwithspartanry.BWMMatConverter;
 import com.chaosbuffalo.betterwithspartanry.BetterWithSpartanry;
 import com.chaosbuffalo.betterwithspartanry.utils;
+import com.oblivioussp.spartanweaponry.api.DamageHelper;
 import com.oblivioussp.spartanweaponry.client.gui.CreativeTabsSW;
 import com.oblivioussp.spartanweaponry.init.ModelRenderRegistry;
-import com.oblivioussp.spartanweaponry.item.*;
-import com.oblivioussp.spartanweaponry.util.DamageHelper;
+import com.oblivioussp.spartanweaponry.item.ItemBattleaxe;
+import com.oblivioussp.spartanweaponry.item.ItemBoomerang;
+import com.oblivioussp.spartanweaponry.item.ItemCrossbow;
+import com.oblivioussp.spartanweaponry.item.ItemDagger;
+import com.oblivioussp.spartanweaponry.item.ItemGreatsword;
+import com.oblivioussp.spartanweaponry.item.ItemHalberd;
+import com.oblivioussp.spartanweaponry.item.ItemHammer;
+import com.oblivioussp.spartanweaponry.item.ItemJavelin;
+import com.oblivioussp.spartanweaponry.item.ItemKatana;
+import com.oblivioussp.spartanweaponry.item.ItemLance;
+import com.oblivioussp.spartanweaponry.item.ItemLongbow;
+import com.oblivioussp.spartanweaponry.item.ItemLongsword;
+import com.oblivioussp.spartanweaponry.item.ItemMace;
+import com.oblivioussp.spartanweaponry.item.ItemParryingDagger;
+import com.oblivioussp.spartanweaponry.item.ItemPike;
+import com.oblivioussp.spartanweaponry.item.ItemRapier;
+import com.oblivioussp.spartanweaponry.item.ItemSaber;
+import com.oblivioussp.spartanweaponry.item.ItemSpear;
+import com.oblivioussp.spartanweaponry.item.ItemThrowingAxe;
+import com.oblivioussp.spartanweaponry.item.ItemThrowingKnife;
+import com.oblivioussp.spartanweaponry.item.ItemWarhammer;
+import com.oblivioussp.spartanweaponry.util.ConfigHandler;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Mod.EventBusSubscriber
 public class ItemRegistryBWSpartanry {
-    public static final Set<BWMMatConverter> MATERIALS_TO_REGISTER = new HashSet<>();
+    public static final Set<BWMMatConverter> MATERIALS_TO_REGISTER = new LinkedHashSet<>();
 
     public static final String SOULFORGE_STEEL = "soulforgedSteel";
     static {
@@ -31,74 +54,113 @@ public class ItemRegistryBWSpartanry {
     public static void registerItems(RegistryEvent.Register<Item> ev) {
 
 
-        Set<Item> item_set = new HashSet<>();
+        Set<Item> item_set = new LinkedHashSet<>();
         for (BWMMatConverter mat : MATERIALS_TO_REGISTER){
-            ItemKatana katana = new ItemKatana("katana_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.KATANA, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(katana, mat.material, "katana_custom");
-            item_set.add(katana);
-            ItemGreatsword greatsword = new ItemGreatsword("greatsword_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.GREATSWORD, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(greatsword, mat.material, "greatsword_custom");
-            item_set.add(greatsword);
-            ItemLongsword longsword = new ItemLongsword("longsword_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.LONGSWORD, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(longsword, mat.material, "longsword_custom");
-            item_set.add(longsword);
-            ItemSaber saber = new ItemSaber("saber_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.SABER, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(saber, mat.material, "saber_custom");
-            item_set.add(saber);
-            ItemRapier rapier = new ItemRapier("rapier_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.RAPIER, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(rapier, mat.material, "rapier_custom");
-            item_set.add(rapier);
-            ItemDagger dagger = new ItemDagger("dagger_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.DAGGER, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(dagger, mat.material, "dagger_custom");
-            item_set.add(dagger);
-            ItemSpear spear = new ItemSpear("spear_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.SPEAR, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(spear, mat.material, "spear_custom");
-            item_set.add(spear);
-            ItemPike pike = new ItemPike("pike_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.PIKE, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(pike, mat.material, "pike_custom");
-            item_set.add(pike);
-            ItemLance lance = new ItemLance("lance_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.LANCE, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(lance, mat.material, "lance_custom");
-            item_set.add(lance);
-            ItemHalberd halberd = new ItemHalberd("halberd_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.HALBERD, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(halberd, mat.material, "halberd_custom");
-            item_set.add(halberd);
-            ItemWarhammer warhammer = new ItemWarhammer("warhammer_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.WARHAMMER, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(warhammer, mat.material, "warhammer_custom");
-            item_set.add(warhammer);
-            ItemHammer hammer = new ItemHammer("hammer_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.HAMMER, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(hammer, mat.material, "hammer_custom");
-            item_set.add(hammer);
-            ItemThrowingAxe throwing_axe = new ItemThrowingAxe("throwing_axe_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.THROWING_AXE, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(throwing_axe, mat.material, "throwing_axe_custom");
-            item_set.add(throwing_axe);
-            ItemThrowingKnife throwing_knife = new ItemThrowingKnife("throwing_knife_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.THROWING_KNIFE, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(throwing_knife, mat.material, "throwing_knife_custom");
-            item_set.add(throwing_knife);
-            ItemLongbow longbow = new ItemLongbow("longbow_" + mat.name, BetterWithSpartanry.MODID, mat.material);
-            ModelRenderRegistry.addItemToRegistry(longbow, mat.material, "longbow_custom");
-            item_set.add(longbow);
-            ItemCrossbow crossbow = new ItemCrossbow("crossbow_" + mat.name, BetterWithSpartanry.MODID, mat.material);
-            ModelRenderRegistry.addItemToRegistry(crossbow, mat.material, "crossbow_custom");
-            item_set.add(crossbow);
-            ItemJavelin javelin = new ItemJavelin("javelin_" + mat.name, BetterWithSpartanry.MODID, mat.material,
-                    DamageHelper.getDamage(DamageHelper.WeaponType.JAVELIN, mat.material.getMaterial().getAttackDamage()));
-            ModelRenderRegistry.addItemToRegistry(javelin, mat.material, "javelin_custom");
-            item_set.add(javelin);
+            if (!ConfigHandler.disableKatana) {
+                ItemSpartanBMKatana katana = new ItemSpartanBMKatana("katana_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(katana, new ResourceLocation(SpartanBM.MODID, "katana_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(katana);
+            }
+            if (!ConfigHandler.disableGreatsword) {
+                ItemSpartanBMGreatsword greatsword = new ItemSpartanBMGreatsword("greatsword_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(greatsword, new ResourceLocation(SpartanBM.MODID, "greatsword_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(greatsword);
+            }
+            if (!ConfigHandler.disableLongsword) {
+                ItemSpartanBMLongsword longsword = new ItemSpartanBMLongsword("longsword_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(longsword, new ResourceLocation(SpartanBM.MODID, "longsword_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(longsword);
+            }
+            if (!ConfigHandler.disableSaber) {
+                ItemSpartanBMSaber saber = new ItemSpartanBMSaber("saber_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(saber, new ResourceLocation(SpartanBM.MODID, "saber_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(saber);
+            }
+            if (!ConfigHandler.disableRapier) {
+                ItemSpartanBMRapier rapier = new ItemSpartanBMRapier("rapier_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(rapier, new ResourceLocation(SpartanBM.MODID, "rapier_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(rapier);
+            }
+            if (!ConfigHandler.disableDagger) {
+                ItemSpartanBMDagger dagger = new ItemSpartanBMDagger("dagger_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(dagger, new ResourceLocation(SpartanBM.MODID, "dagger_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(dagger);
+            }
+            if (!ConfigHandler.disableSpear) {
+                ItemSpartanBMSpear spear = new ItemSpartanBMSpear("spear_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(spear, new ResourceLocation(SpartanBM.MODID, "spear_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(spear);
+            }
+            if (!ConfigHandler.disablePike) {
+                ItemSpartanBMPike pike = new ItemSpartanBMPike("pike_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(pike, new ResourceLocation(SpartanBM.MODID, "pike_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(pike);
+            }
+            if (!ConfigHandler.disableLance) {
+                ItemSpartanBMLance lance = new ItemSpartanBMLance("lance_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(lance, new ResourceLocation(SpartanBM.MODID, "lance_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(lance);
+            }
+            if (!ConfigHandler.disableHalberd) {
+                ItemSpartanBMHalberd halberd = new ItemSpartanBMHalberd("halberd_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(halberd, new ResourceLocation(SpartanBM.MODID, "halberd_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(halberd);
+            }
+            if (!ConfigHandler.disableWarhammer) {
+                ItemSpartanBMWarhammer warhammer = new ItemSpartanBMWarhammer("warhammer_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(warhammer, new ResourceLocation(SpartanBM.MODID, "warhammer_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(warhammer);
+            }
+            if (!ConfigHandler.disableHammer) {
+                ItemSpartanBMHammer hammer = new ItemSpartanBMHammer("hammer_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(hammer, new ResourceLocation(SpartanBM.MODID, "hammer_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(hammer);
+            }
+            if (!ConfigHandler.disableThrowingAxe) {
+                ItemSpartanBMThrowingAxe throwing_axe = new ItemSpartanBMThrowingAxe("throwing_axe_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(throwing_axe, new ResourceLocation(SpartanBM.MODID, "throwing_axe_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(throwing_axe);
+            }
+            if (!ConfigHandler.disableThrowingKnife) {
+                ItemSpartanBMThrowingKnife throwing_knife = new ItemSpartanBMThrowingKnife("throwing_knife_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(throwing_knife, new ResourceLocation(SpartanBM.MODID, "throwing_knife_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(throwing_knife);
+            }
+            if (!ConfigHandler.disableLongbow && !ConfigHandler.woodenLongbowOnly) {
+                ItemSpartanBMLongbow longbow = new ItemSpartanBMLongbow("longbow_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(longbow, new ResourceLocation(SpartanBM.MODID, "longbow_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(longbow);
+            }
+            if (!ConfigHandler.disableCrossbow && !ConfigHandler.woodenCrossbowOnly) {
+                ItemCrossbow crossbow = new ItemSpartanBMCrossbow("crossbow_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(crossbow, new ResourceLocation(SpartanBM.MODID, "crossbow_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(crossbow);
+            }
+            if (!ConfigHandler.disableJavelin) {
+                ItemJavelin javelin = new ItemSpartanBMJavelin("javelin_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(javelin, new ResourceLocation(SpartanBM.MODID, "javelin_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(javelin);
+            }
+            if (!ConfigHandler.disableBattleaxe) {
+                ItemBattleaxe battle_axe = new ItemSpartanBMBattleAxe("battle_axe_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(battle_axe, new ResourceLocation(SpartanBM.MODID, "battle_axe_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(battle_axe);
+            }
+            if (!ConfigHandler.disableBoomerang && !ConfigHandler.woodenBoomerangOnly) {
+                ItemBoomerang boomerang = new ItemSpartanBMBoomerang("boomerang_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(boomerang, new ResourceLocation(SpartanBM.MODID, "boomerang_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(boomerang);
+            }
+            if (!ConfigHandler.disableMace) {
+                ItemMace mace = new ItemSpartanBMMace("mace_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(mace, new ResourceLocation(SpartanBM.MODID, "mace_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(mace);
+            }
+            if (ConfigHandler.enableExperimentalWeapons && !ConfigHandler.disableParryingDagger){
+                ItemParryingDagger parrying_dagger = new ItemSpartanBMParryingDagger("parrying_dagger_" + mat.getName(), mat);
+                ModelRenderRegistry.addItemToRegistry(parrying_dagger, new ResourceLocation(SpartanBM.MODID, "parrying_dagger_custom"), utils.spartanMatFromMMDMat(mat));
+                item_set.add(parrying_dagger);
+            }
 
         }
         for (Item it : item_set){
